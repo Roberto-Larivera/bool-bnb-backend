ffbd0c45bb0440a122573bd29bb5bd3cbbaf5640
Roberto Larivera <robylariv@gmail.com>
2023-04-13 17:04:08 +0200

    Add Migrations

---
+++ database/migrations/2023_04_13_141844_create_views_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('views', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('apartment_id');
+            $table->ipAddress('ip_address');
+            $table->foreign('apartment_id')->references('id')->on('apartments');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('views');
+    }
+};
---
+++ database/migrations/2023_04_13_142421_create_sponsors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sponsors', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',50);
+            $table->unsignedDecimal('price',4,2);
+            $table->unsignedTinyInteger('duration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sponsors');
+    }
+};
