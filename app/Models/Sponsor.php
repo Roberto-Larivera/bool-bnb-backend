85060dfe88d3e9d310c977a8d8d2ff79f7bec05e
Roberto Larivera <robylariv@gmail.com>
2023-04-13 17:03:35 +0200

    Add Models

---
+++ app/Models/Sponsor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsor extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'price',
+        'duration'
+    ];
+
+    public function apartments(){
+        return $this->belongsToMany(Apartment::class);
+    }
+}
