aa1a92d3b1d919bcf41ad5831efc46d5d3dea376
Andrea Raguso <info@andrearaguso.com>
2023-04-15 18:08:47 +0200

    Add un pò di tutto

--- resources/views/admin/messages/index.blade.php
+++ resources/views/admin/messages/index.blade.php
@@ -38,27 +38,35 @@
                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div class="modal-body">
-                        <form action="" class="form-container-small">
+                        <form action="{{ route('admin.messages.index') }}" method="GET" class="form-container-small">
                             <div class="mb-3">
-                                <select class="form-select" aria-label="Default select example">
-                                    <option selected>Appartamenti</option>
-                                    @foreach ($messages->groupBy('apartment.title') as $title => $messagesForTitle)
-                                        <option value="{{ $messagesForTitle->first()->apartment->id }}">
-                                            {{ $title }}
+                                <select class="form-select" aria-label="Default select example" name="apartment_id">
+                                    <option value="">Tutti gli Appartamenti</option>
+                                    @foreach ($apartments as $apartment)
+                                        <option value="{{ $apartment->id }}"
+                                            {{ $selected_apartment_id == $apartment->id ? 'selected' : '' }}>
+                                            {{ $apartment->title }}
                                         </option>
                                     @endforeach
                                 </select>
                             </div>
+                            <div class="">
+                                <div class="form-group">
+                                    <label for="search">Cerca messaggio/mittente:</label>
+                                    <input type="text" class="form-control" name="search" id="search"
+                                        value="{{ $search }}" placeholder="Cerca messaggi...">
+                                </div>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="my-btn rounded" data-bs-dismiss="modal">
+                                    Esci
+                                </button>
+                                <button type="submit" class="my-submit rounded">
+                                    <i class="fa-solid fa-magnifying-glass mx-2"></i><span class="fw-bold fs-6">Cerca</span>
+                                </button>
+                            </div>
                         </form>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="my-btn rounded" data-bs-dismiss="modal">
-                        Esci
-                    </button>
-                    <button type="submit" class="my-submit rounded">
-                        <i class="fa-solid fa-magnifying-glass mx-2"></i><span class="fw-bold fs-6">Cerca</span> 
-                    </button>
                 </div>
             </div>
         </div>
@@ -67,35 +75,51 @@
         @include('admin.partials.success')
         @include('admin.partials.warning')
 
-        <div class="wrapper-messages bg-white py-5 border rounded-4 px-2 px-md-4">
-            @foreach ($messages as $message)
-                <hr class="m-2">
-                <div class="row message">
-                    <div class="col-3 col-sm-2 col-md-2 col-lg-1 message-image text-center">
-                        foto
-                    </div>
-                    <div class="d-none d-md-block col-md-2 col-lg-1 col-1 message-name">
-                        {{ $message->sender_name }}
-                    </div>
-                    <div class="col-9 col-sm-8 col-md-6 col-lg-2 message-object">
-                        <strong
-                            class="d-block d-sm-none">{{ Illuminate\Support\Str::limit($message->object, 18) }}</strong>
-                        <strong class="d-none d-sm-block">{{ $message->object }}</strong>
-                    </div>
-                    <div class="d-none d-lg-block col-md-5 col-lg-7 col-7 message-text">
-                        <p class="d-none d-lg-block">{{ Illuminate\Support\Str::limit($message->sender_text, 110) }}</p>
-                    </div>
-                    <div class="d-none d-sm-block col-sm-1 col-md-1 col-lg-1 col-1 message-time text-end pe-5">
-                        <strong>{{ date('H:i', strtotime($message->created_at)) }}</strong>
-                    </div>
-                </div>
-            @endforeach
-            <hr class="m-2">
-        </div>
-
-
-
-
+        @if ($messages->isNotEmpty())
+            <table class="table my-4 rounded">
+                <thead>
+                    <tr>
+                        <th scope="col">Appartamento</th>
+                        <th scope="col" class="d-none d-md-table-cell">Mittente</th>
+                        <th scope="col" class="d-none d-lg-table-cell">Oggetto</th>
+                        <th scope="col" class="d-none d-lg-table-cell">Messaggio</th>
+                        <th scope="col">Orario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @forelse ($messages as $message)
+                        <tr>
+                            <td>{{ $message->apartment->title }}</td>
+                            <td class="d-none d-md-table-cell">{!! str_replace(
+                                request('search'),
+                                '<span class="highlight">' . request('search') . '</span>',
+                                $message->sender_name,
+                            ) !!} {!! str_replace(
+                                request('search'),
+                                '<span class="highlight">' . request('search') . '</span>',
+                                $message->sender_surname,
+                            ) !!}</td>
+                            <td class="d-none d-lg-table-cell">{!! str_replace(request('search'), '<span class="highlight">' . request('search') . '</span>', $message->object) !!}</td>
+                            <td>
+                                <p class="d-none d-lg-block m-0">
+                                    {!! Illuminate\Support\Str::limit(
+                                        str_replace(request('search'), '<span class="highlight">' . request('search') . '</span>', $message->sender_text),
+                                        80,
+                                    ) !!}
+                                </p>
+                            </td>
+                            <td><strong>{{ date('H:i', strtotime($message->created_at)) }}</strong></td>
+                        </tr>
+                    @empty
+                    @endforelse
+                </tbody>
+            </table>
+        @else
+            <div class="d-flex justify-content-center gap-3 align-items-center fs-4 mt-5">
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <p class="m-0">Non ci sono messaggi da visualizzare...</p>
+            </div>
+        @endif
 
 
     </div>
