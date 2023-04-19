d9065057b1aaa40367a4810773e48901c96d90be
Emanuela Stetsko <emanuelastetsko.es@gmail.com>
2023-04-19 12:21:10 +0200

    Vista show ultimata (manca sezione pagamento)

--- resources/views/admin/sponsors/show.blade.php
+++ resources/views/admin/sponsors/show.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<div class="row row-cols-1 mb-5 my-4 mx-2">
+<div class="row row-cols-1 mb-2 my-4 mx-2">
     <div class="col py-3">
         <h1>
             <span class="icon-section me-2">
@@ -23,41 +23,91 @@
 </div>
 
 <div class="container h-100">
-    <div class="row gy-5 mt-5 mt-md-0 d-flex justify-content-center mb-2">
-        <div class="col-12 col-lg-4 d-flex justify-content-center">
-            <div class="my-card p-4">
-                <h2 class="card-title text-center fw-bold {{ strtolower(explode(' ', $sponsor->title)[1]) }} px-2 py-3 ">{{ explode(' ', $sponsor->title)[1] }}</h2>
-                <h5 class="px-2 py-3 py-xxl-5 text-center">Prezzo: {{ $sponsor->price }} €</h5>
-                <h6 class="px-2 py-3 py-xxl-5 text-center">Durata: {{ $sponsor->duration }} h</h6>
-                <p class="card-text p-2 text-center">{{ $sponsor->description }}</p>
+    <div class="row gy-5 mt-5 mt-md-0 d-flex justify-content-center mb-5">
+        @if (isset($apartments))
+            <div class="col-xs-12 col-xl-4 d-flex justify-content-center">
+                <div class="my-card p-4">
+                    <h2 class="card-title text-center fw-bold {{ strtolower(explode(' ', $sponsor->title)[1]) }} px-2 py-3 ">{{ explode(' ', $sponsor->title)[1] }}</h2>
+                    <h5 class="px-2 py-3 py-xxl-5 text-center">Prezzo: {{ $sponsor->price }} €</h5>
+                    <h6 class="px-2 py-3 py-xxl-5 text-center">Durata: {{ $sponsor->duration }} h</h6>
+                    <p class="card-text p-2 text-center">{{ $sponsor->description }}</p>
+                </div>
             </div>
-        </div>
-    </div>
-    <div class="row gy-5 mt-5 mt-md-0 d-flex justify-content-center mb-3">
-        <div class="col-12 col-lg-4 d-flex justify-content-center">
-            @if (isset($apartments))
-                @foreach ($apartments as $apartment)
-                <div class="my-card mx-3">
-                    <h5 class="mx-3 p-3">{{ $apartment->title }}</h5>
-                    <div class="p-3">
+            <div class="col-xs-12 col-xl-8 d-flex justify-content-center align-items-center">
+                <table class="table my-4 rounded">
+                    <thead>
+                        <tr>
+                            <th scope="col" class="d-none d-md-table-cell">
+                                Foto
+                            </th>
+                            <th scope="col">
+                                Titolo
+                            </th>
+                            <th scope="col" class="d-none d-lg-table-cell">
+                                Indirizzo
+                            </th>
+                            <th scope="col" class="d-none d-xxl-table-cell">
+                                Prezzo / notte
+                            </th>
+                            <th scope="col">
+                                Seleziona
+                            </th>
+                        </tr>
+                    </thead>
+
+                    @foreach ($apartments as $index => $apartment)
+                        <tbody>
+                            <tr>
+                                <td class="d-none d-md-table-cell align-middle">
+                                    <div class="apartment-img-container">
+                                        <img src="{{ $apartment->main_img }}" alt=" {{ $apartment->title }}" class="img-fluid">
+                                    </div>
+                                </td>
+                                <td class="align-middle">
+                                    {{ $apartment->title }}
+                                </td>
+                                <td class="d-none d-lg-table-cell align-middle">
+                                    {{ $apartment->address }}
+                                </td>
+                                <td class="d-none d-xxl-table-cell align-middle">
+                                    {{ $apartment->price }}
+                                </td>
+                                <td class="align-middle">
+                                    <div class="form-check">
+                                        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    @endforeach
+                </table
+            </div>
+        @else
+            <div class="col-md-12 col-lg-6 d-flex justify-content-center">
+                <div class="my-card p-4">
+                    <h2 class="card-title text-center fw-bold {{ strtolower(explode(' ', $sponsor->title)[1]) }} px-2 py-3 ">{{ explode(' ', $sponsor->title)[1] }}</h2>
+                    <h5 class="px-2 py-3 py-xxl-5 text-center">Prezzo: {{ $sponsor->price }} €</h5>
+                    <h6 class="px-2 py-3 py-xxl-5 text-center">Durata: {{ $sponsor->duration }} h</h6>
+                    <p class="card-text p-2 text-center">{{ $sponsor->description }}</p>
+                </div>
+            </div>
+
+            <div class="col-md-12 col-lg-6 d-flex justify-content-center align-items-center">
+                <div class="my-card">
+                    <h2 class="text-center px-2 pt-5 ">{{ $apartment->title }}</h2>
+                    <div class="p-5">
                         <img class="img-fluid rounded" src="{{ $apartment->main_img }}">
                     </div>
                 </div>
-                @endforeach
-            @else 
-                <div class="my-card mx-3">
-                    <h5 class="mx-3 p-3 text-center">{{ $apartment->title }}</h5>
-                    <div class="p-3">
-                        <img class="img-fluid rounded" src="{{ $apartment->main_img }}">
-                    </div>
-                </div>
-            @endif
-        </div>
+            </div>
+                
+        @endif
     </div>
-    <div class="row">
-        <div class="col d-flex justify-content-center">
-            <div class="button d-none d-lg-block mt-3">
-                <a href="{{ route('admin.messages.index',  ['apartment_id' => $apartment->id]) }}" class="secondary-btn me-3">
+    
+    <div class="row row-cols-1">
+        <div class="col-12 d-flex justify-content-center">
+            <div class="button mt-3">
+                <a href="#" class="secondary-btn me-3">
                     Procedi al pagamento
                 </a>
             </div>
