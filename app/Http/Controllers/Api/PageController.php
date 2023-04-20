efa16ed8b8c3f8cf03212cda4279d85accb28482
Andrea Raguso <info@andrearaguso.com>
2023-04-20 13:20:07 +0200

    add show api

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+ 
 
 // Models
 use App\Models\Apartment;
 use App\Models\Service;
+
+use Exception;
 
 class PageController extends Controller
 {
@@ -24,6 +27,27 @@ class PageController extends Controller
     // show apartments ::slug
     public function show($slug)
     {
+
+        try {
+            $apartment = Apartment::where('slug', $slug)->with('services')->firstOrFail();
+    
+            $response = [
+                'success' => true,
+                'code' => 200,
+                'message' => 'OK',
+                'apartment' => $apartment
+            ];
+        } catch (Exception $e) {
+            $response = [
+                'success' => false,
+                'code' => $e->getCode(),
+                'message' => $e->getMessage()
+            ];
+        }
+    
+        return response()->json($response);
+
+
     }
 }
 
