f95bd9020abed49debbcf7ba1a35f6eea4357fcd
Roberto Larivera <robylariv@gmail.com>
2023-04-20 12:02:30 +0200

    inizio api

---
+++ app/Http/Controllers/Api/PageController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+// Models
+use App\Models\Apartment;
+use App\Models\Service;
+
+class PageController extends Controller
+{
+    // home page
+    public function home()
+    {
+
+    }
+
+    // index apartments 
+    public function index()
+    {
+
+    }
+
+    // show apartments ::slug
+    public function show()
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Controllers
+use App\Http\Controllers\Api\PageController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +17,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::name('api.')->group(function (){
+    Route::resource('apartments', PageController::class)->only([
+        'index','show','home'
+    ]);
+
 });
