7e101b9577014c72e0165eb66a920ffecfd358b0
Roberto Larivera <robylariv@gmail.com>
2023-04-20 17:08:25 +0200

    aggiunta api services

--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,8 @@ return [
 
     'allowed_methods' => ['*'],
 
-    // 'allowed_origins' => ['*'],
-
-    'allowed_origins' => [env('APP_FRONTEND_URL','http://localhost:5180')],
+    'allowed_origins' => ['*'],
+    // 'allowed_origins' => [env('APP_FRONTEND_URL','http://localhost:5174')],
 
     'allowed_origins_patterns' => [],
 
