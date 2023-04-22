fc3c9bc3247cba7675aa3f236def2f1b09d9ae0d
Andrea Raguso <info@andrearaguso.com>
2023-04-23 00:09:49 +0200

    finiti filter stats

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -20,12 +20,15 @@ class PageController extends Controller
         $user = Auth::user();
         $allApartments = Apartment::where('user_id', $user->id)->get();
 
-        if($request->has('selectApartment')){
-            $apartments = Apartment::where('id', $request->input('selectApartment'))
-                      ->where('user_id', $user->id)
-                      ->get();
-        }
-        else{
+        if ($request->has('selectApartment')) {
+            if ($request->input('selectApartment') == 'tutti' || $request->input('selectApartment') == '') {
+                $apartments = Apartment::where('user_id', $user->id)->get();
+            } else {
+                $apartments = Apartment::where('id', $request->input('selectApartment'))
+                    ->where('user_id', $user->id)
+                    ->get();
+            }
+        } else {
             $apartments = Apartment::where('user_id', $user->id)->get();
         }
 
@@ -34,31 +37,48 @@ class PageController extends Controller
 
         //Controlli Views
         if ($request->has('month')) {
-            $selectedMonth = $request->input('month');
-            $totalViews = 0;
-            foreach ($apartments as $apartment) {
-                $totalViews += $apartment->views()->whereMonth('created_at', '=', $selectedMonth)->count();
+            if ($request->input('month') == '' || $request->input('month') == 'tutti') {
+                $totalViews = 0;
+                $totalMessages = 0;
+                foreach ($apartments as $apartment) {
+                    $totalMessages += $apartment->messages()->count();
+                }
+                foreach ($apartments as $apartment) {
+                    $totalViews += $apartment->views()->count();
+                }
             }
-        } else {
+            else{
+                $selectedMonth = $request->input('month');
+                $totalViews = 0;
+                $totalMessages = 0;
+                foreach ($apartments as $apartment) {
+                    $totalMessages += $apartment->messages()->whereMonth('created_at', '=', $selectedMonth)->count();
+                }
+                foreach ($apartments as $apartment) {
+                    $totalViews += $apartment->views()->whereMonth('created_at', '=', $selectedMonth)->count();
+                }
+            }
+        } 
+        else 
+        {
             $totalViews = 0;
+            $totalMessages = 0;
+            foreach ($apartments as $apartment) {
+                $totalMessages += $apartment->messages()->count();
+            }
             foreach ($apartments as $apartment) {
                 $totalViews += $apartment->views()->count();
             }
         }
 
+        //Tutte le visualizzazioni per mese
         $viewsByMonth = array_fill(0, 12, 0);
         foreach ($apartments as $apartment) {
             $views = $apartment->views()->whereYear('created_at', '=', date('Y'))->get();
             foreach ($views as $view) {
-                $month = $view->created_at->format('m') - 1; // sottrae 1 per ottenere l'indice dell'array
+                $month = $view->created_at->format('m') - 1;
                 $viewsByMonth[$month]++;
             }
-        }
-
-        //Controlli messaggi
-        $totalMessages = 0;
-        foreach ($apartments as $apartment) {
-            $totalMessages += $apartment->messages()->count();
         }
 
 
@@ -68,59 +88,8 @@ class PageController extends Controller
             'totalViews' => $totalViews,
             'totalMessages' => $totalMessages,
             'viewsByMonth' => $viewsByMonth,
-            'allApartments' => $allApartments
+            'allApartments' => $allApartments,
+            'request' => $request
         ]);
     }
-
-
-
-    // public function dashboard(Request $request)
-    // {
-    //     $user = Auth::user();
-    //     $apartments = Apartment::where('user_id', $user->id)->get();
-
-    //     //Controlli Views
-    //     $selectedApartment = null;
-    //     if ($request->has('selectApartment')) {
-    //         $selectedApartment = Apartment::find($request->input('selectApartment'));
-    //     }
-
-    //     $totalViews = 0;
-    //     if ($selectedApartment) {
-    //         $totalViews = $selectedApartment->views()->count();
-    //     } else {
-    //         if ($request->has('month')) {
-    //             $selectedMonth = $request->input('month');
-    //             foreach ($apartments as $apartment) {
-    //                 $totalViews += $apartment->views()->whereMonth('created_at', '=', $selectedMonth)->count();
-    //             }
-    //         } else {
-    //             foreach ($apartments as $apartment) {
-    //                 $totalViews += $apartment->views()->count();
-    //             }
-    //         }
-    //     }
-
-    //     $viewsByMonth = array_fill(0, 12, 0);
-    //     foreach ($apartments as $apartment) {
-    //         $views = $apartment->views()->whereYear('created_at', '=', date('Y'))->get();
-    //         foreach ($views as $view) {
-    //             $month = $view->created_at->format('m') - 1; // sottrae 1 per ottenere l'indice dell'array
-    //             $viewsByMonth[$month]++;
-    //         }
-    //     }
-
-    //     //Controlli messaggi
-    //     $totalMessages = 0;
-    //     foreach ($apartments as $apartment) {
-    //         $totalMessages += $apartment->messages()->count();
-    //     }
-
-    //     return view('admin.dashboard', [
-    //         'apartments' => $apartments,
-    //         'totalViews' => $totalViews,
-    //         'totalMessages' => $totalMessages,
-    //         'viewsByMonth' => $viewsByMonth
-    //     ]);
-    // }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.admin')
 
 @section('content')
-{{-- QUESTO MESSAGGIO 'SEI LOGGATO' --}}
-{{-- <div class="container-fluid mt-4">
+    {{-- QUESTO MESSAGGIO 'SEI LOGGATO' --}}
+    {{-- <div class="container-fluid mt-4">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -22,181 +22,188 @@
     </div>
 </div> --}}
 
-<div class="container-fluid mt-4">
-    <div class="row row-cols-1 mb-5">
-        <div class="col py-3">
-            <h1>
-                <span class="icon-section">
-                    <i class="fa-solid fa-gauge-high fa-sm"></i>
-                </span>
-                Dashboard
-            </h1>
-        </div>
+    <?php
+    use Illuminate\Http\Request;
+    ?>
+    <div class="container-fluid mt-4">
+        <div class="row row-cols-1 mb-5">
+            <div class="col py-3">
+                <h1>
+                    <span class="icon-section">
+                        <i class="fa-solid fa-gauge-high fa-sm"></i>
+                    </span>
+                    Dashboard
+                </h1>
+            </div>
 
-        <div class="col">
-            <a href="{{ route('admin.dashboard') }}" class="back">
+            <div class="col">
+                <a href="{{ route('admin.dashboard') }}" class="back">
                     Torna indietro
                     <i class="fa-solid fa-rotate-left"></i>
-            </a>
+                </a>
+            </div>
         </div>
     </div>
-</div>
 
-<div class="container-fluid mt-4">
-    <div class="row row-cols-1">
-        <div class="col">
-            <div>
-                Filtra in base all'appartamento e al mese:
-            </div>
-        </div>  
-    </div>
-</div>
-
-<div class="container-fluid">
-    <div class="row row-cols-2 mb-5">
-        <div class="col py-3">
-            <form action="{{ route('admin.dashboard') }}" method="GET" id="formApartment">
-                <select class="form-select" aria-label="Default select example" name="selectApartment"  onchange="document.getElementById('formApartment').submit()">
-                    <option selected>
-                        Seleziona appartamento
-                    </option>
-                    @foreach ($allApartments as $apartment)
-                        <option value="{{ $apartment->id }}">
-                            {{ $apartment->title }}
-                        </option>
-                    @endforeach
-                </select>
-            </form>
-        </div>
-
-        <div class="col py-3">
-            <form action="{{ route('admin.dashboard') }}" method="GET" id="dateFilterForm">
-                <select class="form-select" aria-label="Default select example" name="month" onchange="document.getElementById('dateFilterForm').submit()">
-                    <option selected>Seleziona periodo</option>
-                    <option value="01">Gennaio</option>
-                    <option value="02">Febbraio</option>
-                    <option value="03">Marzo</option>
-                    <option value="04">Aprile</option>
-                    <option value="05">Maggio</option>
-                    <option value="06">Giugno</option>
-                    <option value="07">Luglio</option>
-                    <option value="08">Agosto</option>
-                    <option value="09">Settembre</option>
-                    <option value="10">Ottobre</option>
-                    <option value="11">Novembre</option>
-                    <option value="12">Dicembre</option>
-                </select>
-            </form>
-        </div>
-</div>
-
-
-<div class="container-fluid mt-4">
-    <div class="row justify-content-between row-cols-1 row-cols-md-3 mb-5">
-        <div class="col py-3">
-            <div class="card">
-                <div class="card-body statistics rounded p-3">
-                    <h5 class="card-title my-4">
-                        <i class="fa-solid fa-eye"></i>
-                        Visualizzazioni
-                    </h5>
-                    <h2 class="card-subtitle  text-body-secondary mb-3 fw-bold">
-                        {{ $totalViews }}
-                    </h2>
-                    <p class="card-text mb-4">
-                        Lorem ipsum dolor sit.
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col ps-0 py-3">
-            <div class="card">
-                <div class="card-body statistics rounded p-3">
-                    <h5 class="card-title my-4">
-                        <i class="fa-solid fa-envelope"></i>
-                        Messaggi
-                    </h5>
-                    <h2 class="card-subtitle text-body-secondary mb-3 fw-bold">
-                        {{ $totalMessages }}
-                    </h2>
-                    <p class="card-text mb-4">
-                        Lorem ipsum dolor sit.
-                    </p>
-                </div>
-            </div>
-        </div>
-
-        <div class="col ps-0 py-3">
-            <div class="card">
-                <div class="card-body statistics rounded p-3">
-                    <h5 class="card-title my-4">
-                        <i class="fa-solid fa-sack-dollar"></i>
-                        Guadagno
-                    </h5>
-                    <h2 class="card-subtitle text-body-secondary mb-3 fw-bold">
-                        200.000 &euro;
-                    </h2>
-                    <p class="card-text mb-4">
-                        Lorem ipsum dolor sit.
-                    </p>
+    <div class="container-fluid mt-4">
+        <div class="row row-cols-1">
+            <div class="col">
+                <div>
+                    Filtra in base all'appartamento e al mese:
                 </div>
             </div>
         </div>
     </div>
-</div>
 
-
-<div class="container-fluid mt-4">
-    <div class="row row-cols-1">
-        <div class="col">
-            <div>
-                Visualizzazioni statistiche
+    <div class="container-fluid">
+        <div class="row row-cols-2 mb-5">
+            <div class="col py-3">
+                <form action="{{ route('admin.dashboard') }}" method="GET" id="formApartment">
+                    <select class="form-select" aria-label="Default select example" name="selectApartment"
+                        onchange="document.getElementById('formApartment').submit()">
+                        <option value="tutti" {{ $request->input('selectApartment') == 'tutti' ? 'selected' : '' }}>Tutti gli appartamenti</option>
+                        @foreach ($allApartments as $apartment)
+                            <option value="{{ $apartment->id }}"
+                                {{ $apartment->id == $request->input('selectApartment') ? 'selected' : '' }}>
+                                {{ $apartment->title }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <input type="hidden" name="month" value="{{ $request->input('month') }}">
+                </form>
             </div>
-            <canvas id="viewsChart"></canvas>
-        </div>  
-    </div>
-</div>
+        
+            <div class="col py-3">
+                <form action="{{ route('admin.dashboard') }}" method="GET" id="dateFilterForm">
+                    <select class="form-select" aria-label="Default select example" name="month"
+                        onchange="document.getElementById('dateFilterForm').submit()">
+                        <option value="tutti" {{ $request->input('month') == 'tutti' ? 'selected' : '' }}>Tutti i mesi</option>
+                        <option value="01" {{ $request->input('month') == '01' ? 'selected' : '' }}>Gennaio</option>
+                        <option value="02" {{ $request->input('month') == '02' ? 'selected' : '' }}>Febbraio</option>
+                        <option value="03" {{ $request->input('month') == '03' ? 'selected' : '' }}>Marzo</option>
+                        <option value="04" {{ $request->input('month') == '04' ? 'selected' : '' }}>Aprile</option>
+                        <option value="05" {{ $request->input('month') == '05' ? 'selected' : '' }}>Maggio</option>
+                        <option value="06" {{ $request->input('month') == '06' ? 'selected' : '' }}>Giugno</option>
+                        <option value="07" {{ $request->input('month') == '07' ? 'selected' : '' }}>Luglio</option>
+                        <option value="08" {{ $request->input('month') == '08' ? 'selected' : '' }}>Agosto</option>
+                        <option value="09" {{ $request->input('month') == '09' ? 'selected' : '' }}>Settembre</option>
+                        <option value="10" {{ $request->input('month') == '10' ? 'selected' : '' }}>Ottobre</option>
+                        <option value="11" {{ $request->input('month') == '11' ? 'selected' : '' }}>Novembre</option>
+                        <option value="12" {{ $request->input('month') == '12' ? 'selected' : '' }}>Dicembre</option>
+                    </select>
+                    <input type="hidden" name="selectApartment" value="{{ $request->input('selectApartment') }}">
+                </form>
+            </div>
+        </div>
+        
 
-<script>
 
-var ctx = document.getElementById('viewsChart').getContext('2d');
-var viewsChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre', 'Dicembre'],
-        datasets: [{
-            label: 'Visualizzazioni Totali',
-            data: {!! json_encode($viewsByMonth) !!},
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 5
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
+        <div class="container-fluid mt-4">
+            <div class="row justify-content-between row-cols-1 row-cols-md-3 mb-5">
+                <div class="col py-3">
+                    <div class="card">
+                        <div class="card-body statistics rounded p-3">
+                            <h5 class="card-title my-4">
+                                <i class="fa-solid fa-eye"></i>
+                                Visualizzazioni
+                            </h5>
+                            <h2 class="card-subtitle  text-body-secondary mb-3 fw-bold">
+                                {{ $totalViews }}
+                            </h2>
+                            <p class="card-text mb-4">
+                                Lorem ipsum dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col ps-0 py-3">
+                    <div class="card">
+                        <div class="card-body statistics rounded p-3">
+                            <h5 class="card-title my-4">
+                                <i class="fa-solid fa-envelope"></i>
+                                Messaggi
+                            </h5>
+                            <h2 class="card-subtitle text-body-secondary mb-3 fw-bold">
+                                {{ $totalMessages }}
+                            </h2>
+                            <p class="card-text mb-4">
+                                Lorem ipsum dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col ps-0 py-3">
+                    <div class="card">
+                        <div class="card-body statistics rounded p-3">
+                            <h5 class="card-title my-4">
+                                <i class="fa-solid fa-sack-dollar"></i>
+                                Guadagno
+                            </h5>
+                            <h2 class="card-subtitle text-body-secondary mb-3 fw-bold">
+                                200.000 &euro;
+                            </h2>
+                            <p class="card-text mb-4">
+                                Lorem ipsum dolor sit.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="container-fluid mt-4">
+            <div class="row row-cols-1">
+                <div class="col">
+                    <div>
+                        Visualizzazioni statistiche
+                    </div>
+                    <canvas id="viewsChart"></canvas>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            var ctx = document.getElementById('viewsChart').getContext('2d');
+            var viewsChart = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto',
+                        'Settembre', 'Ottobre', 'Novembre', 'Dicembre'
+                    ],
+                    datasets: [{
+                        label: 'Visualizzazioni Totali',
+                        data: {!! json_encode($viewsByMonth) !!},
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 5
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
                 }
-            }]
-        }
-    }
-});
-</script>
-
-@endsection
+            });
+        </script>
+    @endsection
