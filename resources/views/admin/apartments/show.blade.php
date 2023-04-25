c1b31d4960d5140a566560fc91d3f0aa475f5bdc
Roberto Larivera <robylariv@gmail.com>
2023-04-25 15:31:58 +0200

    int gallery

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -34,7 +34,43 @@
                 {{-- img url  --}}
                 {{-- <img class="img-fluid rounded" src="{{ $apartment->main_img }}"> --}}
                 {{-- img file  --}}
-                <img class="img-fluid rounded" src="{{ $apartment->full_path_main_img }}">
+                <div class="row">
+                    <div class="col">
+                        <img class="img-fluid rounded" src="{{ $apartment->full_path_main_img }}">
+                    </div>
+                </div>
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4">
+                    <div class="col">
+                        {{-- input nascoto --}}
+                        <div class="p-3">
+                            <i class="fa-solid fa-square-plus fa-5x color-primary"></i>
+                        </div>
+                        <input type="file" class="d-none" id="main_img"
+                            name="main_img" accept="image/*">
+
+                        <img id="previewImg" src="#" class="rounded d-none w-100 mb-3 mb-lg-0">
+                    </div>
+                    <div class="col">
+                        {{-- input nascoto --}}
+                        <div class="p-3">
+                            <i class="fa-solid fa-square-plus fa-5x color-primary"></i>
+                        </div>
+                        <input type="file" class="d-none" id="main_img"
+                            name="main_img" accept="image/*">
+
+                        <img id="previewImg" src="#" class="rounded d-none w-100 mb-3 mb-lg-0">
+                    </div>
+                    <div class="col">
+                        {{-- input nascoto --}}
+                        <div class="p-3">
+                            <i class="fa-solid fa-square-plus fa-5x color-primary"></i>
+                        </div>
+                        <input type="file" class="d-none" id="main_img"
+                            name="main_img" accept="image/*">
+
+                        <img id="previewImg" src="#" class="rounded d-none w-100 mb-3 mb-lg-0">
+                    </div>
+                </div>
             </div>
             <div class="info-container col col-sm-6 py-3 py-sm-0">
                 <h2>
@@ -69,15 +105,17 @@
 
                 {{-- Bottoni Tablet messaggi / sponsor --}}
                 <div class="buttons d-none d-lg-block mt-3">
-                   <a href="{{ route('admin.messages.index',  ['apartment_id' => $apartment->id]) }}" class="secondary-btn me-3">
-                       Leggi messaggi
-                   </a>
+                    <a href="{{ route('admin.messages.index', ['apartment_id' => $apartment->id]) }}"
+                        class="secondary-btn me-3">
+                        Leggi messaggi
+                    </a>
 
-                   {{-- Aggiungere rotta sponsor --}}
-                   <a href="{{ route('admin.sponsors.index',  ['apartment_id' => $apartment->id]) }}" class="secondary-btn">
-                       Sponsorizza
-                   </a>
-               </div>
+                    {{-- Aggiungere rotta sponsor --}}
+                    <a href="{{ route('admin.sponsors.index', ['apartment_id' => $apartment->id]) }}"
+                        class="secondary-btn">
+                        Sponsorizza
+                    </a>
+                </div>
             </div>
         </div>
     </div>
@@ -105,7 +143,7 @@
                 </h2>
                 <ul>
                     <li class="ps-2 mb-1">
-                        <i class="fa-solid fa-house"></i> 
+                        <i class="fa-solid fa-house"></i>
                         {{ $apartment->mq }} mq
                     </li>
                     <li class="ps-2 mb-1">
@@ -113,13 +151,13 @@
                         {{ $apartment->rooms }} stanze
                     </li>
                     <li class="ps-2 mb-1">
-                        <i class="fa-solid fa-bed"></i> 
+                        <i class="fa-solid fa-bed"></i>
                         {{ $apartment->beds }} letti
-                     </li>
-                     <li class="ps-2 mb-1">
-                        <i class="fa-solid fa-toilet-paper"></i> 
+                    </li>
+                    <li class="ps-2 mb-1">
+                        <i class="fa-solid fa-toilet-paper"></i>
                         {{ $apartment->baths }} bagni
-                     </li>
+                    </li>
                 </ul>
             </div>
 
@@ -149,7 +187,7 @@
                 <h3>
                     Azioni
                 </h3>
-                 <div class="actions mt-3">
+                <div class="actions mt-3">
                     <a href="{{ route('admin.apartments.edit', $apartment->id) }}" class="primary-btn me-2">
                         <i class="fa-solid fa-pen my-color-dark"></i>
                     </a>
@@ -157,40 +195,44 @@
                     <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST" class="d-inline">
                         @csrf
                         @method('DELETE')
-                    
-                        <button type="button" class="btn-modal primary-btn" data-bs-toggle="modal" data-bs-target="#modal-delete">
+
+                        <button type="button" class="btn-modal primary-btn" data-bs-toggle="modal"
+                            data-bs-target="#modal-delete">
                             <i class="fa-solid fa-trash my-color-dark"></i>
                         </button>
-                    
+
                         <!-- Modal -->
-                        <div class="modal fade" id="modal-delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal fade" id="modal-delete" tabindex="-1" aria-labelledby="exampleModalLabel"
+                            aria-hidden="true">
                             <div class="modal-dialog">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                <h1 class="modal-title fs-5" id="exampleModalLabel">
-                                    Cancella appartamento
-                                </h1>
-                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-body">
-                                    <p>
-                                        Ricorda che se cancelli questo appartamento <strong>tutti i messaggi ricevuti</strong> verranno cancellati.
-                                        <br>
-                                        Vuoi procedere alla cancellazione?
-                                    </p>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="button" class="primary-btn" data-bs-dismiss="modal">
-                                        Chiudi
-                                    </button>
-                                    <button type="submit" class="secondary-btn">
-                                        Cancella
-                                    </button>
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                            Cancella appartamento
+                                        </h1>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>
+                                            Ricorda che se cancelli questo appartamento <strong>tutti i messaggi
+                                                ricevuti</strong> verranno cancellati.
+                                            <br>
+                                            Vuoi procedere alla cancellazione?
+                                        </p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="primary-btn" data-bs-dismiss="modal">
+                                            Chiudi
+                                        </button>
+                                        <button type="submit" class="secondary-btn">
+                                            Cancella
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </form> 
-               </div>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
