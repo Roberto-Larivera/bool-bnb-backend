ab0ea1febc7bf5166e7bd969bbe2c32aa47d7e4b
Roberto Larivera <robylariv@gmail.com>
2023-04-26 19:24:50 +0200

    favicon

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -5,6 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" href="{{ url('assets/favicon.png') }}">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
