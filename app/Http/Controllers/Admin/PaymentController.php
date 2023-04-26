1c90b01a7c100ea677a38ad707518e01200e0231
Andrea Raguso <info@andrearaguso.com>
2023-04-26 17:49:16 +0200

    add email

--- app/Http/Controllers/Admin/PaymentController.php
+++ app/Http/Controllers/Admin/PaymentController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use Braintree\Transaction;
 use Illuminate\Support\Facades\Auth;
 
+use Illuminate\Support\Facades\Mail;
+use App\Mail\payment;
+
 use App\Models\Apartment;
 use App\Models\Sponsor;
 use App\Models\ApartmentSponsor;
@@ -47,18 +50,20 @@ class PaymentController extends Controller
 
     public function process(Request $request)
     {
-
-                        
-
-
         $payload = $request->input('payload', false);
         $nonce = $payload['nonce'];
         $sponsorId = request()->input('sponsor');
         $apartmentId = request()->input('apartment');
         $sponsor = Sponsor::where('id',$sponsorId)->first();
-
-
-
+        $apartment = Sponsor::where('id',$apartmentId)->first();
+        $hostEmail = $apartment->user->email;
+        $data = [
+            'apartment_name' => $apartment->title,
+            'sponsor_price' => $sponsor->price,
+            'sponsor_duration' => $sponsor->duration,
+            'sponsor_name' => $sponsor->title,
+            'object' => 'ciao tante care cose'
+        ];
         $status = Transaction::sale([
             'amount' => $sponsor->price,
             'paymentMethodNonce' => $nonce,
@@ -70,15 +75,13 @@ class PaymentController extends Controller
         $currentDate = date("Y-m-d H:i:s");
         $currentDateMin = date("Y-m-d H:i:s", strtotime('+'.$sponsor->duration.'hours', strtotime($currentDate)));
 
-        
-
         if($status){
+            Mail::to($hostEmail)->send(new payment($data));
             $apartment_sponsor = new ApartmentSponsor();
             $apartment_sponsor->apartment_id = $apartmentId;
             $apartment_sponsor->sponsor_id = $sponsorId;
             $apartment_sponsor->deadline = $currentDateMin;
             $apartment_sponsor->save();
-
         }
 
         return response()->json($status);
