f7cb44c53d34b77b2bacb2f67e193d997fa5457d
“Francesca <dascanio.francesca@gmail.com>
2023-04-28 12:20:46 +0200

    ultimi dettagli

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -30,9 +30,9 @@
 
     <div class="container-fluid mt-4">
         <div class="row">
-            <div class="d-flex flex-column justify-content-lg-between align-items-lg-center">
+            <div class="d-flex flex-column justify-content-lg-between align-items-lg-center p-0 p-lg-1">
                     {{-- suddivisione principale in col-11 --}}
-                <div class="col-11 main-show">
+                <div class="col-12 main-show">
                     <div class="row">
                         <div class="col">
                             <h2>
@@ -44,32 +44,39 @@
                     <div class="row">
                         <div class="col">
                             <p class="mt-2">
-                                <span class="pe-2">
-                                    {{ $apartment->address }}  <strong>|</strong> 
-                                </span>
-                                <span class="pe-2">
+                                <div class="pe-2 d-block d-sm-inline">
+                                    {{ $apartment->address }}  
+                                    <span class="d-none d-sm-inline">
+                                        <strong>|</strong>
+                                    </span> 
+                                </div>
+                                <div class="pe-2 d-block d-sm-inline">
                                     @if ($apartment->visible == 1)
                                         Pubblico
                                     @elseif ($apartment->visible == 0)
                                         Privato
                                     @endif
-                                    <strong>|</strong> 
-                                </span>
-                                <span>
+                                    <span class="d-none d-sm-inline">
+                                        <strong>|</strong>
+                                    </span> 
+                                </div>
+                                <div class="d-block d-sm-inline">
                                     &euro; {{ $apartment->price }}
-                                </span>
+                                </div>
                             </p>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-9 img-container">
+                        <div class="col img-container">
                             <div class="row pt-4 pb-2">
-                                <div class="col">
+                                <div class="col-12 col-lg-9">
                                     <img class="img-fluid rounded" src="{{ $apartment->full_path_main_img }}">
                                 </div>
                             </div>
-                            <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-6">
+
+
+                            <div class="row row-cols-2 row-cols-md-2 row-cols-lg-6 row-cols-xl-6">
                                 @if($imageGallery != [])
                                 @forEach($imageGallery as $image_gallery)
                                 <div class="col mb-3">
@@ -87,7 +94,7 @@
                                 @endforeach
                                 @endif
                                 @if(count($imageGallery) < 4)
-                                <div class="col align-self-center">
+                                <div class="col align-self-center mb-3">
                                     
                                     <div class="add-img">
                                         <button class="plus cursor-copy primary-btn" onclick="selectFile()">
@@ -119,7 +126,7 @@
                         </div>
 
 
-                        <div class="col-10 col-md-6 col-lg-3 mb-4">
+                        <div class="col-10 col-md-6 col-lg-6 mb-4">
                             <h2 class="mb-3">
                                 Dettagli
                             </h2>
@@ -151,7 +158,7 @@
                             @if (count($apartment->services) > 0)
                                 <div class="services-container">
                                     @foreach ($apartment->services as $service)
-                                        <span class="my-badge bg-light me-2">
+                                        <span class="my-badge bg-light">
                                             {{ $service->name }}
                                         </span>
                                     @endforeach
@@ -165,19 +172,69 @@
                 </div>
 
                 {{-- suddivisione principale in col-1 --}}
-                <div class="col-12 col-lg-1 aside-show rounded">
-                    <div class="d-flex justify-content-between flex-lg-column align-items-lg-center p-3">
-                        <div class="aside-edit text-end py-4">
-                            <i class="fa-regular fa-pen-to-square fa-2xl"></i>
+                <div class="col-12 col-lg-1 aside-show rounded-top">
+                    <div class="d-flex justify-content-between flex-lg-column align-items-lg-center px-3 py-1 rounded-lg">
+                        <div class="aside-edit text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Modifica appartamento">
+                            <a href="{{ route('admin.apartments.edit', $apartment->id) }}">
+                                <i class="fa-regular fa-pen-to-square fa-xl"></i>
+                            </a>
                         </div>
-                        <div class="aside-delete text-end py-4">
-                            <i class="fa-solid fa-trash fa-2xl"></i>
+                        <div class="aside-delete text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Cancella appartamento">
+                            <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST" class="d-inline">
+                                @csrf
+                                @method('DELETE')
+        
+                                <button type="button" class="btn-modal" data-bs-toggle="modal" data-bs-target="#modal-delete" style="border: 0; background-color: transparent;">
+                                    <i class="fa-solid fa-trash fa-xl"></i>
+                                </button>
+
+                                {{-- Modale --}}
+                                <div class="modal fade" id="modal-delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">
+                                                    Cancella appartamento
+                                                </h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <p>
+                                                    Ricorda che se cancelli questo appartamento <strong>tutti i messaggi
+                                                        ricevuti</strong> verranno cancellati.
+                                                    <br>
+                                                    Vuoi procedere alla cancellazione?
+                                                </p>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="primary-btn" data-bs-dismiss="modal">
+                                                    Chiudi
+                                                </button>
+                                                <button type="submit" class="secondary-btn">
+                                                    Cancella
+                                                </button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
-                        <div class="aside-sponsor text-end py-4">
-                            <i class="fa-solid fa-sack-dollar fa-2xl"></i>
+                        <div class="aside-sponsor text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Sponsorizza">
+                            @if($apartment->sponsored == false)
+                                <a href="{{ route('admin.sponsors.index',  ['apartment_id' => $apartment->id]) }}">
+                                    <i class="fa-solid fa-sack-dollar fa-xl"></i>
+                                </a>
+                            @else
+                                <a class="disabled-link">
+                                    <i class="fa-solid fa-sack-dollar fa-xl"></i>
+                                </a>
+                            @endif
                         </div>
-                        <div class="aside-messages text-end py-4">
-                            <i class="fa-solid fa-envelope fa-2xl"></i>
+                        <div class="aside-messages text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Leggi messaggi">
+                            <a href="{{ route('admin.messages.index', ['apartment_id' => $apartment->id]) }}">
+                                <i class="fa-solid fa-envelope fa-xl"></i>
+                            </a>
                         </div>
                     </div>
                 </div>
