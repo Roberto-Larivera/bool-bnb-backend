98321d553af5e4885c63067705e5aaf7b5b213f2
“Francesca <dascanio.francesca@gmail.com>
2023-04-28 09:00:57 +0200

    stilizzazione responsive aside show

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -30,9 +30,9 @@
 
     <div class="container-fluid mt-4">
         <div class="row">
-            <div class="d-flex justify-content-between align-items-center">
-                 {{-- suddivisione principale in col-11 --}}
-                <div class="col-11">
+            <div class="d-flex flex-column justify-content-lg-between align-items-lg-center">
+                    {{-- suddivisione principale in col-11 --}}
+                <div class="col-11 main-show">
                     <div class="row">
                         <div class="col">
                             <h2>
@@ -63,7 +63,7 @@
                     </div>
 
                     <div class="row">
-                        <div class="col img-container">
+                        <div class="col-9 img-container">
                             <div class="row pt-4 pb-2">
                                 <div class="col">
                                     <img class="img-fluid rounded" src="{{ $apartment->full_path_main_img }}">
@@ -108,7 +108,18 @@
                     </div>
 
                     <div class="row pt-4">
-                        <div class="col-2">
+
+                        <div class="col-12 col-lg-11 mb-4">
+                            <h2>
+                                Descrizione
+                            </h2>
+                            <p>
+                                {{ $apartment->description }}
+                            </p>
+                        </div>
+
+
+                        <div class="col-10 col-md-6 col-lg-3 mb-4">
                             <h2 class="mb-3">
                                 Dettagli
                             </h2>
@@ -132,16 +143,8 @@
                             </ul>
                         </div>
 
-                        <div class="col-5">
-                            <h2>
-                                Descrizione
-                            </h2>
-                            <p>
-                                {{ $apartment->description }}
-                            </p>
-                        </div>
                         
-                        <div class="col-5">
+                        <div class="col-12 col-lg-8 mb-4">
                             <h2 class="mb-3">
                                 Servizi
                             </h2>
@@ -162,8 +165,8 @@
                 </div>
 
                 {{-- suddivisione principale in col-1 --}}
-                <div class="col-1">
-                    <div class="d-flex flex-column align-items-center p-3 rounded" style="background-color: rgba(221, 204, 224, 0.5); margin-left: 50%;">
+                <div class="col-12 col-lg-1 aside-show rounded">
+                    <div class="d-flex justify-content-between flex-lg-column align-items-lg-center p-3">
                         <div class="aside-edit text-end py-4">
                             <i class="fa-regular fa-pen-to-square fa-2xl"></i>
                         </div>
@@ -179,8 +182,7 @@
                     </div>
                 </div>
             </div>
-
-            </div>
+        </div>
     </div>
 
     {{-- codice precedente --}}
