5a625723518929aba9c033e7b152c3658de715b6
“Francesca <dascanio.francesca@gmail.com>
2023-05-01 21:24:40 +0200

    prova responsive tooltips

--- resources/views/admin/apartments/show.blade.php
+++ resources/views/admin/apartments/show.blade.php
@@ -205,11 +205,13 @@
                 {{-- suddivisione principale in col-1 --}}
                 <div class="col-12 col-lg-1 aside-show rounded-top">
                     <div class="d-flex justify-content-between flex-lg-column align-items-lg-center px-3 py-1 rounded-lg">
-                        <div class="aside-edit text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Modifica appartamento">
-                            <a href="{{ route('admin.apartments.edit', $apartment->id) }}">
+                        {{-- <div class="aside-edit text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Modifica appartamento"> --}}
+                        <div class="py-4">
+                            <a href="{{ route('admin.apartments.edit', $apartment->id) }}" class="aside-edit" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Modifica appartamento">
                                 <i class="fa-regular fa-pen-to-square fa-xl"></i>
                             </a>
                         </div>
+                        {{-- </div> --}}
                         <div class="aside-delete text-end py-4" data-bs-toggle="tooltip" data-bs-placement="left" data-bs-title="Cancella appartamento">
                             <form action="{{ route('admin.apartments.destroy', $apartment->id) }}" method="POST" class="d-inline">
                                 @csrf
